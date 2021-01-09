c28d7ebd2a93fffab75eb5f86bb19a4525231492
nick@deploy.at <mikowhy@tutanota.com>
2021-01-09 22:03:12 +0100

    feat: style wallet manager

--- dashboard/src/components/wallet.js
+++ dashboard/src/components/wallet.js
@@ -51,19 +51,71 @@ export const Wallet = observer((props) => {
   }
 
   return (
-    <div className="column is-8-desktop has-text-centered mt-5 is-shady">
-      <div>
+    <div className="column is-9-desktop mt-5 p-5 is-shady">
       {transactions &&
         transactions.map((tx, i) => (
           <div key={i}>
-            <p>HDWallet addres: {tx.to}</p>
-            <p>Address from: {tx.from}</p>
-            <p>Value: {tx.value}</p>
-            <p>Date: {tx.timestamp}</p>
-            <br />
+            <div className="columns is-mobile is-vcentered">
+              <div className="column">
+                <div class="field is-grouped is-grouped-multiline">
+                  <div class="control">
+                    <div class="tags has-addons">
+                      <span class="tag">Block number</span>
+                      <span class="tag is-link">{tx.blockNumber}</span>
+                    </div>
+                  </div>
+                  <div class="control">
+                    <div class="tags has-addons">
+                      <span class="tag">Confirmations</span>
+                      <span class="tag is-warning">{tx.confirmations} / 12</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="columns is-mobile is-vcentered">
+              <div className="column is-3">
+                <p className="has-text-weight-bold">Tx hash:</p>
+              </div>
+              <div className="column">
+                <p className="tag is-warning is-light">{tx.hash}</p>
+              </div>
+            </div>
+            <div className="columns is-mobile is-vcentered">
+              <div className="column is-3">
+                <p className="has-text-weight-bold">HDWallet addres:</p>
+              </div>
+              <div className="column">
+                <p className="tag is-warning is-light is-medium">{tx.to}</p>
+              </div>
+            </div>
+            <div className="columns is-mobile is-vcentered">
+              <div className="column is-3">
+                <p className="has-text-weight-bold">Address from:</p>
+              </div>
+              <div className="column">
+                <p className="tag is-warning is-light is-medium">{tx.from}</p>
+              </div>
+            </div>
+            <div className="columns is-mobile is-vcentered">
+              <div className="column is-3">
+                <p className="has-text-weight-bold">ETH value:</p>
+              </div>
+              <div className="column">
+                <p className="tag is-warning is-light is-medium">{tx.value}</p>
+              </div>
+            </div>
+            <div className="columns is-mobile is-vcentered">
+              <div className="column is-3">
+                <p className="has-text-weight-bold">Date:</p>
+              </div>
+              <div className="column">
+                <p className="tag is-warning is-light is-medium">{tx.timestamp}</p>
+              </div>
+            </div>
+            <hr />
           </div>
         ))}
-        </div>
     </div>
   )
 })
@@ -82,17 +134,21 @@ const getTransactionsByAccount = async (web3, account, startBlockNumber, endBloc
     console.log(`Searching block ${i} / ${endBlockNumber}`)
 
     let block = await web3.eth.getBlock(i, true)
-    let number = block.number
+    let blockNumber = block.number
 
     if (block != null && block.transactions != null) {
       for (let tx of block.transactions) {
         if (account == tx.to) {
-          console.log('Transaction found on block: ' + number)
+          console.log('Transaction found on block: ' + blockNumber)
+          const confirmations = endBlockNumber - blockNumber
           const txObj = {
+            hash: tx.hash,
             to: tx.to,
             from: tx.from,
             value: web3.utils.fromWei(tx.value, 'ether'),
             timestamp: moment(block.timestamp, 'X').format('DD MM YYYY HH:mm:ss'),
+            blockNumber,
+            confirmations,
           }
           result.push(txObj)
         }
@@ -100,7 +156,7 @@ const getTransactionsByAccount = async (web3, account, startBlockNumber, endBloc
     }
   }
 
-  return result
+  return result.reverse()
 }
 
 // await web3.eth.sendTransaction({ from: "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1", to: '0x24D294fc35Ed473FD5d976C71AA79f39A28Fae3F', value: web3.utils.toWei('0.4', "ether")})
